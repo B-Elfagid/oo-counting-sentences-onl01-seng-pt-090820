2c7710a31c024dc6c2cb6f4226248409fba09823
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-09-27 16:46:21 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -10,13 +10,12 @@ class String
     self.end_with?("?")
  end
 
-  def exclamation?
-    self.end_with?("!")
-  end
+    def exclamation?
+      self.end_with?("!")
+    end
+
   
-   def count_sentences
-    array = self.split(/[.!?] /)
-    array.length
-  end 
+      
+  
 
 end
